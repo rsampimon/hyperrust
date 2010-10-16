446e585df301ce586f14514710edc77fed7fdf40
rsampimon <rsampimon@sampimon.net>
2010-10-16 18:17:27 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hyperrust::Application.config.secret_token = 'da4e76799fc6712425f2df0639bb5d8599c1bb93a3d9183b30050a9e14801f2ccfab2c7b12cc71155d1da9f8a56cb4c4b26830afe709ff5484543f4c5cdac709'

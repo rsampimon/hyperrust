4f60c30edf0e2d71353c57acfeaa1a43591c6fff
rsampimon <rsampimon@sampimon.net>
2010-11-12 23:59:38 +1100

    First Import

---
+++ public/Assets/hyperrust/content.php
@@ -0,0 +1,51 @@
+<div>
+	<img src="img/header-rotm.jpg" alt="rustie of the month"/>
+</div>
+
+<h1>What's it all about?</h1>
+
+<p><b>Rustie of the Month</b> was created in April of 1998 by Sue Green as a way to recognize the contributions of the people that make the Rust List what it is. Any Rustie may nominate any other Rustie that they feel deserves public recognition for good deeds, exceptional activities, or just plain good Rustie vibrations.</p>
+<p>In early 1999 Lauren Clodi assumed the role of coordinator of the poll and dilligently managed it through 2000. In 2001, Chris Horn took over the role of coordinator of the poll and continued to dilligently manage it through mid 2002. In mid 2002, Bob Ballerstein took on the task of gathering the nominations and tabulating the votes. Bob passed this task on to Sharry Wilson in October 2004.</p>
+<p>Nominations are opened early each month. If you'd like to nominate a Rustie, first be sure that the person hasn't already held the title (see list below). If not, just <a href="mailto:sharry@interlog.com">drop Sharry a line</a> and let her know who you think deserves the recognition. Please include a good description of why you would like to nominate the person, their real name if possible (unless you only know their handle) and their email address.</p>
+<p>Please include a sentence or two in your nomination that can be posted publicly to tell the rest of the Rusties why this person deserves the distinction.</p>
+<p>About a week before the end of each month, Sharry will post to Rust the names of those who have been nominated for that month's honor. At that point any Rustie can vote for their choice from among the nominees. If you choose to vote, put the name of the person you wish to vote for in the subject line of a note and address the email to Sharry at <a href="mailto:sharry@interlog.com">sharry@interlog.com</a>. The vote must be placed before midnight of the last day of the month.</p>
+<p>Sharry will announce the <b>Rustie of the Month</b> on the Rust List as soon as the votes have been tallied.</p>
+
+<h1>What does the RotM do?</h1>
+<p>Nothing really. Just sit back and bask in the glory. :-)<p> 
+<p>Actually, there is one traditional "duty": the outgoing <b>RotM</b> will greet the new <b>Rustie of the Month</b> by sending his or her trade list for a complimentary selection.</p> 
+
+<h1>Past RotMs</h1>
+
+<table class="rotm">
+	<tr>
+		<td>
+			<img src="img/rotm-kaz.jpg"/>
+			<br/>
+			Kaz
+		</td>
+		<td>
+			<img src="img/rotm-robert.jpg"/>
+			<br/>
+			Robert
+		</td>
+		<td>
+			<img src="img/rotm-fredrik.jpg"/>
+			<br/>
+			Fredrik
+		</td>
+	<tr>
+	<tr>
+		<td>
+			<img src="img/rotm-johnny.jpg"/>
+			<br/>
+			Johnny
+		</td>
+		<td>
+			<img src="img/rotm-roel.jpg"/>
+			<br/>
+			Roel
+		</td>
+	<tr>
+</table>
+
---
+++ public/Assets/hyperrust/right.php
@@ -0,0 +1,19 @@
+<div id="neck">
+<div class="neck1"></div>
+<div class="neck2"></div>
+<div class="neck3"></div>
+</div>
+<div id="menu">
+	<div class="menuitem">
+		<div class="bg"></div>
+		<div class="text">
+			<a href="#"><img src="img/menu-home.png"/></a>
+		</div>
+	</div>
+	<div class="menuitem">
+		<div class="bg"></div>
+		<div class="text">
+			<a href="#"><img src="img/menu-rust.png"/></a>
+		</div>
+	</div>
+</div>

690e0ba49e402dcde8d1cef0b181fb878e28d090
Nathan Sampimon <nathan@inspire9.com>
2010-11-26 00:13:14 +1100

    bug fix with navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     navs.each do |nav|
      if nav.active
         contents = ''
-        contents << content_tag(:a, nav.name.html_safe, :href => public_page_url(nav))
+        contents << content_tag(:a, nav.name.html_safe, :href => public_page_url(nav.slug))
         # contents << "&nbsp;"
         contents << content_tag(:ul, nav_items(nav.children).html_safe) if nav.children.any?
         all_contents << content_tag(:li, contents.html_safe, :id => "page_#{nav.id}")

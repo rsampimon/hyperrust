be6d51a4134f50622783c29a5528bf16eb57a406
Nathan Sampimon <nathan@inspire9.com>
2010-11-26 00:10:35 +1100

    fix stub issue

--- app/models/page.rb
+++ app/models/page.rb
@@ -6,6 +6,6 @@ class Page < ActiveRecord::Base
   acts_as_list
   acts_as_tree
   
-  validates_presence_of :name, :stub
-  validates_uniqueness_of :stub
+  validates_presence_of :name, :slug
+  validates_uniqueness_of :slug
 end
